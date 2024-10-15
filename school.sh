e7539e90f46edef630e45afb824900b435dc01bf
Xupeng Tang <xtang254@wisc.edu>
2024-10-15 15:48:27 -0500

    Revise school

--- school.sh
+++ school.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+grep "MADISON SCHOOLS" Property_Tax_Roll.csv | cut -d, -f7 | {
+    sum=0
+    count=0
+    while read value; do
+        sum=$((sum + value))
+        count=$((count + 1))
+    done
+    echo "Total Assessed Value: $sum"
+    echo "Average Assessed Value: $((sum / count))"
+}
