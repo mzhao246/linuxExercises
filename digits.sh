28697160ef3a64a7cf3a236490b83c970b8c52eb
xiangsen dong <xdong95@wisc.edu>
2024-10-15 14:19:02 -0500

    revise digits

--- digits.sh
+++ digits.sh
@@ -1,1 +1,10 @@
-#!/bin/bash
+sum=0
+
+for num in {1000..2000}
+do
+    if [[ $num =~ ^[01]+$ ]]; then
+        sum=$((sum + num))
+    fi
+done
+
+echo "The sum of numbers between 1000 and 2000 that consist only of digits 0 and 1 is: $sum"
